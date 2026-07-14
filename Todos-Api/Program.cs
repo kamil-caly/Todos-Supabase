28fddbe4321f0ed8b0e4e35febc495752f558f80
Kamil Cały <KCaly@sygnity.pl>
2026-07-14 12:36:50 +0200

    Revert "break the correct domain in coors for test"
    This reverts commit dacf23cd4d8a04560d5bfd0c86113b6d54f807aa.

--- Todos-Api/Program.cs
+++ Todos-Api/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     });
     options.AddPolicy(AllowLocalhostAndProdPolicy, policy =>
     {
-        policy.WithOrigins("https://kamil-caly.github.i", "http://localhost:3000")
+        policy.WithOrigins("https://kamil-caly.github.io", "http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
